cdb290fb248b12a6971e4b2291155a568aeb31cf
SamLev-2004 <samlevkovsky@gmail.com>
2024-11-18 10:09:05 -0500

    Delete Projects directory

--- Projects/CalendarProject/SQLEvents.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE events (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    event_title TEXT,
-    date TEXT NOT NULL,
-    description TEXT,
-    time TEXT NOT NULL,
-    category TEXT
-);
